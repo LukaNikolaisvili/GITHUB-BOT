93906469470724956e8a3ac74821162533284ff5
LukaNikolaisvili <jokerpoker1213@gmail.com>
2024-08-03 16:17:24 +0000

    added run and index.js files

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 # Define the repository URL and directory
 REPO_URL="https://github.com/LukaNikolaisvili/GITHUB-BOT.git"
-REPO_DIR="/var/jenkins_home/workspace/GITHUB-BOT/GITHUB-BOT"
+REPO_DIR="/var/jenkins_home/workspace/test/GITHUB-BOT"
 
 # Ensure the repository directory is safe for Git
 git config --global --add safe.directory $REPO_DIR
@@ -14,11 +14,22 @@ if [ ! -d "$REPO_DIR" ]; then
 else
   echo "Repository already exists. Pulling latest changes..."
   cd $REPO_DIR || exit 1
+  
+  # Check for unmerged files
+  if git ls-files -u | grep -q '^'; then
+    echo "Unmerged files detected. Please resolve conflicts manually."
+    exit 1
+  fi
+  
+  git config pull.rebase false # Configure git to use merge strategy
   git pull
 fi
 
 # Navigate to the repository directory
 cd $REPO_DIR || { echo "Failed to change directory to $REPO_DIR"; exit 1; }
 
+# Install required Node.js modules
+npm install
+
 # Execute the Node.js script
-node index.js
+node /var/jenkins_home/workspace/GITHUB_BOT/GITHUB-BOT/index.js
